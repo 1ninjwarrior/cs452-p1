81731bb4d0e91747c31199bb41aa8fb5866ce1b8
1ninjwarrior <jadendawdy@gmail.com>
2024-09-25 11:30:28 -0600

    Task 3 done

--- app/main.c
+++ app/main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "../src/lab.h"
 
-int main(void)
-{
-  printf("hello world\n");
-  return 0;
+int main(int argc, char *argv[]) {
+    int opt;
+
+    while ((opt = getopt(argc, argv, "v")) != -1) {
+        switch (opt) {
+            case 'v':
+                print_version();
+                exit(EXIT_SUCCESS);
+            default:
+                fprintf(stderr, "Usage: %s [-v]\n", argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    printf("Shell is running...\n");
+    return 0;
 }
--- src/lab.c
+++ src/lab.c
@@ -1,1 +1,7 @@
 /**Update this file with the starter code**/
+#include "lab.h"
+#include <stdio.h>
+
+void print_version(void) {
+    printf("Shell version %d.%d\n", SHELL_VERSION_MAJOR, SHELL_VERSION_MINOR);
+}
